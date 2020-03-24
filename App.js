7f368bac9f0379defff360a12132fcb6674825fe
CAM603 <cameronhawley@comcast.net>
2020-03-24 19:00:02 -0400

    ADD layout

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput 
+          placeholder="Goal" 
+          style={styles.input}
+        />
+        <Button title="ADD"/>
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  input: {
+    borderBottomColor: 'black', 
+    borderBottomWidth: 1, 
+    padding: 10, 
+    width: '80%'
+  },
+  inputContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center'
+  }
 });
