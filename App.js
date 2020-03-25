4b1174f6a75443b36c43f34b2fe5b21e5c76aea7
CAM603 <cameronhawley@comcast.net>
2020-03-24 20:19:09 -0400

    ADD flatlist

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [goals, setGoals] = useState([])
+
+  const goalInputHandler = (text) => {
+    setEnteredGoal(text)
+  }
+
+  const addGoalHandler = () => {
+    // setGoals([...goals, enteredGoal])
+    setGoals(currentGoals => [
+      ...currentGoals, {id: Math.random().toString(), text: enteredGoal}
+    ]);
+  }
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput 
           placeholder="Goal" 
           style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
         />
-        <Button title="ADD"/>
+        <Button 
+          title="ADD"
+          onPress={addGoalHandler}
+        />
       </View>
-      <View>
-
-      </View>
+      <FlatList
+        data={goals}
+        renderItem={item => (
+          <View style={styles.listItem}>
+            <Text>{item.item.text}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -32,5 +56,12 @@ const styles = StyleSheet.create({
     flexDirection: 'row', 
     justifyContent: 'space-between', 
     alignItems: 'center'
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#BADA22',
+    borderColor: 'black',
+    borderWidth: 1
   }
 });
